ff6fccb39369d1dcd91c6818f54a42481854556c
anyua <anyuanzhi@live.com>
2016-10-19 18:56:09 +0800

    修改完成;

--- src/protest2/Test.java
+++ src/protest2/Test.java
@@ -1,139 +1,113 @@
 package protest2;
 import java.util.Scanner; 
 
-class Exspression //存储输入的字符串
-{		StringBuffer a;
-		
-	public StringBuffer in()
-	{	Scanner input=new Scanner(System.in);
-		StringBuffer a=new StringBuffer();
-		a.append(input.nextLine());
-
-		return a;
+class Exspression {
+	//存储输入的字符串
+	//private StringBuffer thisExpression;
+	public StringBuffer in() {
+		Scanner input = new Scanner(System.in);
+		StringBuffer thisExpression = new StringBuffer();
+		thisExpression.append(input.nextLine());
+		//input.close();
+		return thisExpression;
 	}
 	
 }
 
-class simplify//处理多项式进行代值，求导。
-{		StringBuffer m;
-	public	String out(StringBuffer str)
-	{		
-			
-		
-			//输入多项式
-		if((str.indexOf("!simplify")==-1)&&(str.indexOf("!d/d")==-1))
-			{
-							//str.in(); 
-							m = new StringBuffer(str);
-							return str.toString();
-			}
-		
+class Simplify {
+	//处理多项式进行代值，求导。
+	private StringBuffer thisExpression;
+	public	String out(final StringBuffer  inputString) {
+		final int ten = 10;
+		final int twelve = 12;
+		final int four = 4;
+		//输入多项式
+		int searchSimplify = inputString.indexOf("!simplify");
+		int searchDx = inputString.indexOf("!d/d");
+		if ((searchSimplify == -1) 
+				&& (searchDx == -1)) {
+			//inputString.in(); 
+			thisExpression = new StringBuffer(inputString);
+			return inputString.toString();
+		} else if ((searchSimplify != -1) 
+				&& (searchDx == -1)) {
 			//代值运算
-		else if((str.indexOf("!simplify")!=-1)&&(str.indexOf("!d/d")==-1))
-			{				String n=new String();
-							n=m.toString().replace(str.toString().toCharArray()[10],
-								str.toString().toCharArray()[12]);
-							return n;
-				
-			}
+			String n;
+			n = thisExpression.toString().replace(
+					inputString.toString().toCharArray()[ten],
+					inputString.toString().toCharArray()[twelve]
+							);
+			return n;
+		} else if ((searchSimplify == -1) 
+				&& (searchDx != -1)) { 
 			//求导运算
-		else if((str.indexOf("!simplify")==-1)&&(str.indexOf("!d/d")!=-1))
-			{ 				
-							String o=new String();
-							String fstr[]=new String[10];
-							int q1=0;
-							StringBuffer w=new StringBuffer();
-							StringBuffer wout=new StringBuffer();
-							o=m.toString();
-							
-					for(int i=0;i<=o.length();i++)
-					{		
-						if(i==o.length()){
-							fstr[q1]=w.toString();
-							w.setLength(0);
-							q1++;
-						}
-						else if(o.toCharArray()[i]!='+')
-						{
-							w.append(o.toCharArray()[i]);
-						}
-						else
-						{	
-							fstr[q1]=w.toString();
-							w.setLength(0);
-							q1++;
-						}
-					}
-					
-					boolean flag=false;
-					for(int j=0;j<q1;j++)
-					{		
-							int q2=0;
-					
-							for(int d=0;d<fstr[j].length();d++)
-							{	
-								if(fstr[j].toCharArray()[d]==str.toString().toCharArray()[4])
-								{
-									q2++;
-								}
-							}
-							if(q2>0)
-							{
-								flag=true;
-								for(int d=0;d<fstr[j].length();d++)
-								{	
-									if(fstr[j].toCharArray()[d]==str.toString().toCharArray()[4])
-									{
-										
-													String s = (new Integer(q2)).toString();
-
-										            for(int k=0;k<fstr[j].length();k++)
-										            {
-										            	if(k!=d){
-										            	wout.append(fstr[j].toCharArray()[k]);
-										            	}
-										            	else{
-										            		wout.append(s);
-										            	}
-										            }
-										            
-										            wout.append("+");
-										            break;
-										            
-									}
-								}
-							}
-							
-					}
-					if(!flag)
-						//如果错误；
-					{
-						return "error,no variable!";
-					}
-					return wout.deleteCharAt(wout.length()-1).toString();
-							
-			
+			String nowExpression = null;
+			String[] fstr = new String[ten];
+			int numOfAdd = 0;
+			StringBuffer tempString = new StringBuffer();
+			StringBuffer resultExpression = new StringBuffer();
+			nowExpression = thisExpression.toString();
+			for (int i = 0; i <= nowExpression.length(); i++) {		
+				if (i == nowExpression.length()) {
+					fstr[numOfAdd] = tempString.toString();
+					tempString.setLength(0);
+					numOfAdd++;
+				} else if (nowExpression.toCharArray()[i] != '+') {
+					tempString.append(nowExpression.toCharArray()[i]);
+				} else {	
+					fstr[numOfAdd] = tempString.toString();
+					tempString.setLength(0);
+					numOfAdd++;
+				}
 			}
+			boolean flag = false;
+			for (int j = 0; j < numOfAdd; j++) {		
+				int numOfVar = 0;
+				for (int d = 0; d < fstr[j].length(); d++) {	
+					if (fstr[j].toCharArray()[d] 
+							== inputString.toString().toCharArray()[four]) {
+						numOfVar++;
+					}
+				}
+				if (numOfVar > 0) {
+					flag = true;
+					for (int d = 0; d < fstr[j].length(); d++) {	
+						if (fstr[j].toCharArray()[d] 
+								== inputString.toString().toCharArray()[four]) {
+							String numOfPow = (
+									Integer.valueOf(numOfVar)).toString();
+				            for (int k = 0; k < fstr[j].length(); k++) {
+				            	if (k != d) {
+				            resultExpression.append(fstr[j].toCharArray()[k]);
+				            	} else {
+				            		resultExpression.append(numOfPow);
+				            	}
+				            }  
+				            resultExpression.append('+');
+				            break;            
+						}
+					}
+				}	
+			}
+			if (!flag) {
+				return "error,no variable!";
+			}
+			return resultExpression.deleteCharAt(
+					resultExpression.length() - 1).toString();	
+		}
 		return null;
 	}
 }
 
 public class Test {
-	public static void main(String[] args)
-	{		
-			simplify result=new simplify();
-			
-			while(true)
-			{	String outstr=new String();
-				Exspression Form=new Exspression();
-				
-				outstr=result.out(Form.in());
-				System.out.println(outstr);
-			}
-			
-	
-			
-	
+	public static void main(final String[] args) {		
+		Simplify result = new Simplify();
 		
+		while (System.in != null) {	
+			String outstr;
+			Exspression form = new Exspression();
+			outstr = result.out(form.in());
+			System.out.println(outstr); // NOPMD by DYN on 16-10-11 下午8:44
+		}	
 	}
 }
